7e5bc39ea7efa114ce5860e155da20e294c2b793
SABBIR <sabbirto13@gmail.com>
2021-03-24 23:19:51 +0600

    Recap Firebase Authentication

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCQAe3F8bSHr-sjKyyhr4sxchr2w-ZPXNM",
+    authDomain: "burj-al-arab-bd58a.firebaseapp.com",
+    projectId: "burj-al-arab-bd58a",
+    storageBucket: "burj-al-arab-bd58a.appspot.com",
+    messagingSenderId: "333706180677",
+    appId: "1:333706180677:web:256ec912f182015dfa4b2c"
+  };
+
+  export default firebaseConfig;            
